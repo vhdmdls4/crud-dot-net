05cd0a331d7fd641c536273ac7b9af441c73c1ca
Victor <vhduartelima@hotmail.com>
2025-03-22 22:07:05 -0300

    feat: book n loan configuration added

---
+++ DatabaseContext/Configurations/BookConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using crud_dot_net.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace crud_dot_net.DatabaseContext.Configurations;
+
+public class BookConfiguration : IEntityTypeConfiguration<Book>
+{
+    public void Configure(EntityTypeBuilder<Book> builder)
+    {
+        builder.ToTable("Books")
+            .HasKey(x => x.Id);
+        builder.Property(x => x.Author)
+            .HasMaxLength(50)
+            .IsRequired();
+        builder.Property(x => x.Title)
+            .HasMaxLength(150)
+            .IsRequired();
+        builder.Property(x => x.Quantity)
+            .IsRequired();
+        builder.Property(x => x.Uuid)
+            .IsRequired();
+        builder.Property(x => x.Summary)
+            .HasMaxLength(500)
+            .IsRequired();
+        builder.Property(x => x.PublishDate)
+            .IsRequired();
+    }
+}
---
+++ DatabaseContext/Configurations/LoanConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using crud_dot_net.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace crud_dot_net.DatabaseContext.Configurations;
+
+public class LoanConfiguration : IEntityTypeConfiguration<Loan>
+{
+    public void Configure(EntityTypeBuilder<Loan> builder)
+    {
+        builder.ToTable("Loans")
+            .HasKey(x => x.Id);
+        builder.Property(x => x.BookId)
+            .IsRequired();
+        builder.Property(x => x.UserId)
+            .IsRequired();
+        builder.Property(x => x.StartDate)
+            .IsRequired();
+        builder.Property(x => x.EndDate)
+            .IsRequired();
+        builder.Property(x => x.Returned)
+            .IsRequired();
+    }
+}

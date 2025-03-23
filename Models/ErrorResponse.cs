0285798d0de865d6dde12ec21ca29e4e4a059481
Victor <vhduartelima@hotmail.com>
2025-03-22 22:07:56 -0300

    feat: ErrorResponse.cs added

---
+++ Models/ErrorResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace crud_dot_net.Models;
+
+public class ErrorResponse
+{
+    public string Message { get; set; }
+    public string Title { get; set; }
+    public int StatusCode { get; set; }
+    
+}

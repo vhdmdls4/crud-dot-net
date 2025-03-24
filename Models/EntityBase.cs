989e60725dd5a71d19ae6495ee939b1d69a179fa
Victor <vhduartelima@hotmail.com>
2025-03-23 22:39:16 -0300

    feat: added entity base

---
+++ Models/EntityBase.cs
@@ -0,0 +1,13 @@
+﻿namespace crud_dot_net.Models;
+
+public abstract class EntityBase
+{
+    public Guid uuid { get; set; }
+    public DateTimeOffset Created { get; set; }
+    public DateTimeOffset LastModified { get; set; }
+    
+    public void UpdateLastModified()
+    {
+        LastModified = DateTimeOffset.UtcNow;
+    }
+}

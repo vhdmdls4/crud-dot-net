a30d3eab54f189bb0e0ca83e04d418fdad977ab6
Victor <vhduartelima@hotmail.com>
2025-03-23 22:39:00 -0300

    feat: added create book request

---
+++ DTO/CreateBookRequest.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace crud_dot_net.DTO;
+
+public class CreateBookRequest
+{
+    [Required]
+    public string Uuid { get; set; }
+    [Required]
+    [StringLength(150)]
+    public string Title { get; set; }
+    [Required]
+    [StringLength(150)]
+    public string Author { get; set; }
+    [StringLength(500)]
+    public string Summary { get; set; }
+    public DateTime PublishDate { get; set; }
+    public int Quantity { get; set; }
+}

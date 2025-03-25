e6c61c9215204367622f0eedc8ada7c76472f1cc
Victor <vhduartelima@hotmail.com>
2025-03-24 23:07:48 -0300

    feat: added auto mapper for createbookrequest to book

---
+++ Mapping/AutoMapperProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using crud_dot_net.DTO;
+using crud_dot_net.Models;
+
+namespace crud_dot_net.Mapping;
+
+public class AutoMapperProfile : Profile
+{
+    public AutoMapperProfile()
+    {
+        {
+            CreateMap<CreateBookRequest, Book>()
+                .ForMember(book => book.Id, opt => opt.Ignore())
+                .ForMember(book => book.Uuid, opt => opt.MapFrom(src => Guid.Parse(src.Uuid)))
+                .ForMember(book => book.Title, opt => opt.MapFrom(src => src.Title))
+                .ForMember(book => book.Author, opt => opt.MapFrom(src => src.Author))
+                .ForMember(book => book.Summary, opt => opt.MapFrom(src => src.Summary))
+                .ForMember(book => book.PublishDate, opt => opt.MapFrom(src => src.PublishDate))
+                .ForMember(book => book.Quantity, opt => opt.MapFrom(src => src.Quantity));
+        }
+    }
+}

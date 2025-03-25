24947f5685be393086a099f5cd865d445e909df2
Victor <vhduartelima@hotmail.com>
2025-03-24 23:10:26 -0300

    feat: added IBookRepository.cs and IBookService.cs

---
+++ Repository/IBookRepository.cs
@@ -0,0 +1,12 @@
+﻿using crud_dot_net.Models;
+
+namespace crud_dot_net.Repository;
+
+public interface IBookRepository
+{
+    Task<Book?> GetByIdAsync(long id);
+    Task<List<Book?>> GetAllAsync();
+    Task AddAsync(Book book);
+    Task UpdateAsync(Book book);
+    Task DeleteAsync(long id);
+}
---
+++ Services/IBookService.cs
@@ -0,0 +1,13 @@
+﻿using crud_dot_net.DTO;
+using crud_dot_net.Models;
+
+namespace crud_dot_net.Services;
+
+public interface IBookService
+{
+    Task<Book?> GetByIdAsync(long id);
+    Task<List<Book?>> GetAllAsync();
+    Task AddAsync(CreateBookRequest request);
+    Task UpdateAsync(long id, CreateBookRequest request);
+    Task DeleteAsync(long id);
+}

f0dfb5d29ed244ad5c9e160d471396a7876219eb
Victor <vhduartelima@hotmail.com>
2025-03-24 23:09:54 -0300

    feat: added BookRepository.cs

---
+++ Repository/Impl/BookRepository.cs
@@ -0,0 +1,47 @@
+﻿using crud_dot_net.DatabaseContext;
+using crud_dot_net.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace crud_dot_net.Repository.Impl;
+
+public class BookRepository : IBookRepository
+{
+    private readonly AppDbContext _context;
+
+    public BookRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task<Book?> GetByIdAsync(long id)
+    {
+        return await _context.Books.FindAsync(id);
+    }
+
+    public async Task<List<Book?>> GetAllAsync()
+    {
+        return await _context.Books.ToListAsync();
+    }
+
+    public async Task AddAsync(Book book)
+    {
+        await _context.Books.AddAsync(book);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(Book book)
+    {
+        _context.Books.Update(book);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(long id)
+    {
+        var book = await _context.Books.FindAsync(id);
+        if (book != null)
+        {
+            _context.Books.Remove(book);
+            await _context.SaveChangesAsync();
+        }
+    }
+}

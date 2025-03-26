73beb37c414d009954657b7ddb63080761d48786
Victor <vhduartelima@hotmail.com>
2025-03-26 00:12:29 -0300

    feat: added entity base config

---
+++ DatabaseContext/Configurations/EntityBaseConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using crud_dot_net.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace crud_dot_net.DatabaseContext.Configurations;
+
+public class EntityBaseConfiguration<T> : IEntityTypeConfiguration<T> where T : EntityBase
+{
+    public virtual void Configure(EntityTypeBuilder<T> builder)
+    {
+        builder.Property(x => x.Uuid)
+            .IsRequired();
+
+        builder.Property(x => x.Created)
+            .IsRequired()
+            .HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+        builder.Property(x => x.LastModified)
+            .IsRequired()
+            .HasDefaultValueSql("CURRENT_TIMESTAMP");
+    }
+}

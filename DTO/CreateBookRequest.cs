5f50d5c29f79c46e46a4effd1971fdcdeddca2ea
Victor <vhduartelima@hotmail.com>
2025-03-26 00:12:59 -0300

    update: adjusted CreateBookRequest.cs

--- DTO/CreateBookRequest.cs
+++ DTO/CreateBookRequest.cs
@@ -2,18 +2,19 @@
 
 namespace crud_dot_net.DTO;
 
-public class CreateBookRequest
+public record CreateBookRequest(string Title, string Author, string Summary, DateTime PublishDate, int Quantity)
 {
     [Required]
-    public string Uuid { get; set; }
+    [StringLength(150)]
+    public string Title { get; set; } = Title;
+
     [Required]
     [StringLength(150)]
-    public string Title { get; set; }
-    [Required]
-    [StringLength(150)]
-    public string Author { get; set; }
+    public string Author { get; set; } = Author;
+
     [StringLength(500)]
-    public string Summary { get; set; }
-    public DateTime PublishDate { get; set; }
-    public int Quantity { get; set; }
+    public string Summary { get; set; } = Summary;
+
+    public DateTime PublishDate { get; set; } = PublishDate;
+    public int Quantity { get; set; } = Quantity;
 }

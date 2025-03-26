1cf1c7e62a4cd68cb9d60e812139141231d022b6
Victor <vhduartelima@hotmail.com>
2025-03-26 00:13:42 -0300

    refactor: simplify AppDbContext constructor by removing DbSettings dependency

--- DatabaseContext/AppDbContext.cs
+++ DatabaseContext/AppDbContext.cs
@@ -6,21 +6,15 @@ namespace crud_dot_net.DatabaseContext;
 
 public class AppDbContext : DbContext
 {
-    //store connection string
-    private readonly DbSettings _settings;
-
-    //inject dbsettings model through constructor
-    public AppDbContext(IOptions<DbSettings> settings)
-    {
-        _settings = settings.Value;
-    }
-    
-    //represents the dbset property
     public DbSet<Book> Books { get; set; }
     public DbSet<User> Users { get; set; }
     public DbSet<Loan> Loans { get; set; }
     
-    //config model book entity
+    public AppDbContext(DbContextOptions<AppDbContext> options) 
+        : base(options)
+    {
+    }
+    
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);

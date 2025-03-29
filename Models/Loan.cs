872c5ddd01bfe507e3052e98d8e93c7fb541095e
Victor <vhduartelima@hotmail.com>
2025-03-28 23:57:41 -0300

    feat: add foreign key attributes and make EndDate nullable in Loan model

--- Models/Loan.cs
+++ Models/Loan.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace crud_dot_net.Models;
 
@@ -17,13 +18,15 @@ public class Loan
     [Key]
     public long Id { get; set; }
     
-    public Book Book { get; set; }
     public long BookId { get; set; }
+    [ForeignKey("BookId")]
+    public Book Book { get; set; }
     
     public long UserId { get; set; }
+    [ForeignKey("UserId")]
     public User User { get; set; }
     
     public DateTime StartDate { get; set; }
-    public DateTime EndDate { get; set; }
+    public DateTime? EndDate { get; set; }
     public bool Returned { get; set; }
 }

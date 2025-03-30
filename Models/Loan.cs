36c753575b4c6059d266571b224b4320eb114076
Victor <vhduartelima@hotmail.com>
2025-03-29 23:55:40 -0300

    refactor: update Loan constructor to include Book and User parameters for improved initialization

--- Models/Loan.cs
+++ Models/Loan.cs
@@ -5,25 +5,28 @@ namespace crud_dot_net.Models;
 
 public class Loan
 {
-    public Loan(long bookId, long userId, DateTime startDate)
+    public Loan(long bookId, long userId, DateTime startDate, Book book, User user)
     {
         BookId = bookId;
         UserId = userId;
         StartDate = startDate;
+        Book = book;
+        User = user;
         Returned = false;
     }
 
     [Key]
     public long Id { get; set; }
-    
+    [Required] 
     public long BookId { get; set; }
     [ForeignKey("BookId")]
+    [Required]
     public Book Book { get; set; }
-    
+    [Required]
     public long UserId { get; set; }
     [ForeignKey("UserId")]
     public User User { get; set; }
-    
+    [Required]
     public DateTime StartDate { get; set; }
     public DateTime? EndDate { get; set; }
     public bool Returned { get; set; }
